5998adb952e28d0952db6d404f5db6e609156044
Marcos Demetry <marcos.demetry@me.com>
2021-04-04 14:09:44 +0200

    cleaned up targets

--- _targets.R
+++ _targets.R
@@ -1,20 +1,11 @@
 library(targets)
-
-library(data.table)
-library(dplyr)
-library(ggplot2)
-library(gganimate)
-library(gifski)
-library(transformr)
 source("src/functions.R")
 
-tar_option_set(packages = c("data.table", "foreign", "readxl", "ggplot2"))
+tar_option_set(packages = c("data.table", "foreign", "readxl", "ggplot2", "gganimate", "gifski", "transformr", "dplyr"))
 
 list(
-    tar_target(raw_data,
-        read_excel("data/raw_data.xlsx")
-    ),
-    tar_target(processed_data, process_data(raw_data)),
-    tar_target(line_animation, create_line_gif(processed_data)),
-    tar_target(bar_animation, create_bar_gif(processed_data))
+    tar_target(raw_data, "data/raw_data.xlsx", format = "file"),
+
+    tar_target(processed_data, process_data(read_excel(raw_data))),
+    tar_target(line_animation, create_line_gif(processed_data))
 )
